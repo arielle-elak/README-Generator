aee4694232c27bd7e6cba469ee0f8a25c56df9c4
Ari-chan <arielle.elak@gmail.com>
2022-10-03 00:58:07 -0600

    Desired variables set in gM.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,15 +2,33 @@
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  } else {
+    return licenseBadge;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  } else {
+    return licenseURL;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  } else {
+    return licenseURL;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
