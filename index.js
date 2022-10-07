e0e9f83bfb31322f08ac44e3bee2329225036b1b
Arielle Schlickman-Elak <arielle.elak@itg.team>
2022-10-07 16:45:04 -0600

    Reworked licences object

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // DONE: Include packages needed for this application
 const inquirer = require('inquirer');
+// DONE: Create a function to write README file
 const { writeFile } = require('fs').promises;
 var generateMarkdown = require('./utils/generateMarkdown')
 
@@ -68,103 +69,10 @@ const promptUser = () => {
     ]);
 };
 
-// Licenses array with name, image url, license url
-const licenses = [
-    {
-      licenseTitle: 'Apache License 2.0',
-      badgeImg: 'https://img.shields.io/badge/License-Apache_2.0-blue.svg',
-      badgeURL: 'https://opensource.org/licenses/Apache-2.0'
-    },
-    {
-      licenseTitle: 'Boost Software License 1.0',
-      badgeImg: 'https://img.shields.io/badge/License-Boost_1.0-lightblue.svg',
-      badgeURL: 'https://www.boost.org/LICENSE_1_0.txt'
-    },
-     {
-      licenseTitle: 'BSD 2-Clause "Simplified" License',
-      badgeImg: 'https://img.shields.io/badge/License-BSD_2--Clause-orange.svg',
-      badgeURL: 'https://opensource.org/licenses/BSD-2-Clause'
-    },
-    {
-      licenseTitle: 'BSD 3-Clause "New" or "Revised" License',
-      badgeImg: 'https://img.shields.io/badge/License-BSD_3--Clause-blue.svg',
-      badgeURL:'https://opensource.org/licenses/BSD-3-Clause'
-    },
-    {
-      licenseTitle: 'Creative Commons Zero v1.0 Universal',
-      badgeImg: 'https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg',
-      badgeURL: 'http://creativecommons.org/publicdomain/zero/1.0/'
-    },
-    {
-      licenseTitle: 'Eclipse Public License 2.0',
-      badgeImg: 'https://img.shields.io/badge/License-EPL_1.0-red.svg',
-      badgeURL: 'https://opensource.org/licenses/EPL-1.0'
-    },
-    {
-      licenseTitle: 'GNU Affero General Public License v3.0',
-      badgeImg: 'https://img.shields.io/badge/License-AGPL_v3-blue.svg',
-      badgeURL: 'https://www.gnu.org/licenses/agpl-3.0'
-    },
-    {
-      licenseTitle: 'GNU General Public License v3.0',
-      badgeImg: 'https://img.shields.io/badge/License-GPLv3-blue.svg',
-      badgeURL: 'https://www.gnu.org/licenses/gpl-3.0'
-    },
-    {
-      licenseTitle: 'MIT License',
-      badgeImg: 'https://img.shields.io/badge/License-MIT-yellow.svg',
-      badgeURL: 'https://opensource.org/licenses/MIT'
-    },
-    {
-      licenseTitle: 'Mozilla Public License 2.0',
-      badgeImg: 'https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg',
-      badgeURL: 'https://opensource.org/licenses/MPL-2.0'
-    },
-    {
-      licenseTitle: 'The Unlicense',
-      badgeImg: 'https://img.shields.io/badge/license-Unlicense-blue.svg',
-      badgeURL: 'http://www.wtfpl.net/about/'
-    }
-  ]
 
 
-// DONE: Create a function to write README file
-// TODO: Implement export function from generateMarkdown to select licenceTitle,
-// badgeURL and badgeImg
-const generateREADME = ({ licenseTitle, licenseBadge, licenseURL, title, description, installation, usage, contribution, test }) =>
 
-` # ${title}
-[![License](${licenseBadge})](${licenseURL})
 
-## Table of Contents
-
-* [Description](#description)
-* [Installation](#installation)
-* [Usage](#usage)
-* [Contributing](#contributing)
-* [Tests](#tests)
-* [License](#license)
-
-## Description
-${description}
-
-## Installation
-${installation}
-
-## Usage
-${usage}
-
-## Contributing
-${contribution}
-
-## Tests
-${test}
-
-## License
-${licenseTitle}
-${licenseURL}
-
-`;
 
 
 
@@ -172,7 +80,7 @@ ${licenseURL}
 const init = () => {
     promptUser()
         // Use writeFile method imported from fs.promises to use promises instead of a callback function
-        .then((answers) => writeFile('./generated-readme/README.md', generateREADME(answers)))
+        .then((answers) => writeFile('./generated-readme/README.md', generatemarkdown(answers)))
         .then(() => console.log('Successfully wrote to new README file'))
         .catch((err) => console.error(err));
 };
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,79 @@
-// TODO: Pass in current license value from inquirer "license" name prompt
+// Licenses array with name, image url, license url
+
+const licenses =
+{
+  "Apache License 2.0":
+  {
+    "badgeImg": "https://img.shields.io/badge/License-Apache_2.0-blue.svg",
+    "badgeURL": "https://opensource.org/licenses/Apache-2.0"
+  },
+
+  "Boost Software License 1.0":
+  {
+    "badgeImg": "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg",
+    "badgeURL": "https://www.boost.org/LICENSE_1_0.txt"
+  },
+
+  "BSD 2-Clause 'Simplified' License":
+  {
+    "badgeImg": "https://img.shields.io/badge/License-BSD_2--Clause-orange.svg",
+    "badgeURL": "https://opensource.org/licenses/BSD-2-Clause"
+  },
+
+  "BSD 3-Clause 'New' or 'Revised' License":
+  {
+    "badgeImg": "https://img.shields.io/badge/License-BSD_3--Clause-blue.svg",
+    "badgeURL": "https://opensource.org/licenses/BSD-3-Clause"
+  },
+
+  "BSD 3-Clause 'New' or 'Revised' License":
+  {
+    "badgeImg": "https://img.shields.io/badge/License-BSD_3--Clause-blue.svg",
+    "badgeURL": "https://opensource.org/licenses/BSD-3-Clause"
+  },
+
+  "Creative Commons Zero v1.0 Universal":
+  {
+    "badgeImg": "https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg",
+    "badgeURL": "http://creativecommons.org/publicdomain/zero/1.0/"
+  },
+
+  "Eclipse Public License 2.0":
+  {
+    "badgeImg": "https://img.shields.io/badge/License-EPL_1.0-red.svg",
+    "badgeURL": "https://opensource.org/licenses/EPL-1.0"
+  },
+
+  "GNU Affero General Public License v3.0":
+  {
+    "badgeImg": "https://img.shields.io/badge/License-AGPL_v3-blue.svg",
+    "badgeURL": "https://www.gnu.org/licenses/agpl-3.0"
+  },
+
+  "GNU General Public License v3.0":
+  {
+    "badgeImg": "https://img.shields.io/badge/License-GPLv3-blue.svg",
+    "badgeURL": "https://www.gnu.org/licenses/gpl-3.0"
+  },
+
+  "MIT License":
+  {
+    "badgeImg": "https://img.shields.io/badge/License-MIT-yellow.svg",
+    "badgeURL": "https://opensource.org/licenses/MIT"
+  },
+
+  "Mozilla Public License 2.0":
+  {
+    "badgeImg": "https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg",
+    "badgeURL": "https://opensource.org/licenses/MPL-2.0"
+  },
+
+  "The Unlicense":
+  {
+    "badgeImg": "https://img.shields.io/badge/license-Unlicense-blue.svg",
+    "badgeURL": "http://www.wtfpl.net/about/"
+  },
+}
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
@@ -6,6 +81,7 @@ function renderLicenseBadge(license) {
   if (!license) {
     return '';
   } else {
+
     return licenseBadge;
   }
 }
@@ -31,10 +107,48 @@ function renderLicenseSection(license) {
 }
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown() {
+  renderLicenseSection(license);
+  renderLicenseLink(license);
+  renderLicenseBadge(license)
+
+
+
+  const generateREADME = ({ title, description, installation, usage, contribution, test, licence }) =>
+` # ${title}
+[![License](${licenseBadge})](${licenseURL})
+
+## Table of Contents
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [License](#license)
+
+## Description
+${description}
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## Contributing
+${contribution}
+
+## Tests
+${test}
+
+## License
+${licenseTitle}
+${licenseURL}
 
 `;
 }
+
+
 
 module.exports = generateMarkdown;
